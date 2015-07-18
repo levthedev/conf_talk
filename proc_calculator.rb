bfb6e08f944ffac2c68958a157563bc6781d79bb
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-18 15:16:52 -0600

    working for 4 symbols

--- proc_calculator.rb
+++ proc_calculator.rb
@@ -1,8 +1,24 @@
 class Calculator
-  def add(*args)
-    puts :+.to_proc.call(*args)
+  def add(*tuple)
+    p :+.to_proc.call(*tuple)
+  end
+
+  def subtract(*tuple)
+    p :-.to_proc.call(*tuple)
+  end
+
+  def multiply(*tuple)
+    p :*.to_proc.call(*tuple)
+  end
+
+  def divide(*tuple)
+    p :/.to_proc.call(*tuple)
   end
 end
 
 c = Calculator.new
-c.add(1, 2)
+c.add 10, 2
+c.subtract 10, 2
+c.multiply 10, 2
+c.divide 10, 2
+
