e235848df589037f10b7fc19183ede108fa75456
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-18 20:53:09 -0600

    passing tests for count, find, find all, map, to_a

--- enum.rb
+++ enum.rb
@@ -26,19 +26,6 @@ module MyEnumerable
     each { |e| array << block.call(e) }
     array
   end
-
-  def group_by(&block)
-  end
-
-  def each_with_object(object, &block)
-  end
-
-  def each_with_index(&block)
-  end
-
-  def include?(item)
-    each { |e| return false unless block.call(e)}
-  end
 end
 
 RSpec.describe 'MyEnumerable' do
@@ -95,62 +82,9 @@ RSpec.describe 'MyEnumerable' do
     assert_enum(ary, :find_all, [2, 2, 6]) { |i| i.even? }
   end
 
-    specify 'map returns an array of elements that have been passed through the block' do
-      assert_enum([],         :map,         []) { 1 }
-      assert_enum(['a', 'b'], :map,     [1, 1]) { 1 }
-      assert_enum(['a', 'b'], :map, ['A', 'B']) { |char| char.upcase }
-    end
-
-  #  specify 'group_by returns a hash whose keys come from the block, and whose values are the elements we passed to the block, in array' do
-  #    assert_enum([*1..5], :group_by, {true => [1,3,5], false => [2,4]}) { |n| n.odd? }
-  #    assert_enum(['abc', 'lo', 'lol', 'lkjs'], :group_by, {3 => ['abc', 'lol'], 2 => ['lo'], 4 => ['lkjs']}) { |str| str.length }
-  #  end
-  #
-    specify 'include? returns true if the item is in the collection' do
-      assert_enum [],     :include?, 1, false
-      assert_enum [1],    :include?, 1, true
-      assert_enum [2, 1], :include?, 1, true
-      assert_enum [1, 2], :include?, 1, true
-
-      assert_enum [1],    :include?, 3, false
-      assert_enum [2, 1], :include?, 3, false
-    end
-
-  #  describe 'each_with_index' do
-  #    specify 'each_with_index hands the block each item from the collection, and also the index, starting at zero' do
-  #      seen = []
-  #      MyArray.new([]).each_with_index { |e, i| seen << e << i }
-  #      expect(seen).to eq []
-  #
-  #      seen = []
-  #      MyArray.new(['a','b','c']).each_with_index { |e, i| seen << e << i }
-  #      expect(seen).to eq ['a', 0, 'b', 1, 'c', 2]
-  #    end
-  #
-  #    it 'returns the collection iterated over' do
-  #      my_ary = MyArray.new([])
-  #      expect(my_ary.each_with_index { }).to equal my_ary
-  #
-  #      my_ary = MyArray.new([1])
-  #      expect(my_ary.each_with_index { }).to equal my_ary
-  #    end
-  #  end
-  #
-  #  specify 'each_with_object hands the block the item, and an object, and returns the object' do
-  #    assert_enum([], :each_with_object, '', '') { }
-  #
-  #    assert_enum(['a', 'b'], :each_with_object, '', 'ab') do |char, string|
-  #      string << char
-  #      nil
-  #    end
-  #  end
+  specify 'map returns an array of elements that have been passed through the block' do
+    assert_enum([],         :map,         []) { 1 }
+    assert_enum(['a', 'b'], :map,     [1, 1]) { 1 }
+    assert_enum(['a', 'b'], :map, ['A', 'B']) { |char| char.upcase }
+  end
 end
-
-# maybe bonus:
-#   :sort_by,
-#   :each_slice,
-#   :each_cons,
-#   :zip,
-#   :take_while,
-#   :drop_while,
-
